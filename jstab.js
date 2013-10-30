13c8c0dc43415b96bc25574feb83d8da1d71fe7c
Timothy Lin <lzh9102@gmail.com>
2013-10-30 21:33:11 +0800

    initial commit

---
+++ jstab.js
@@ -0,0 +1,35 @@
+var jstab_init = function() {
+
+	var init_tabarea = function(container) {
+		var tab_container = $('<div class="jstab_tab_container"></div>');
+		var switchToPage = function(index) {
+			$('.jstab_page', container).hide();
+			$('.jstab_page:eq(' + index + ')', container).show();
+			$('.jstab_tab', container).removeClass('active');
+			$('.jstab_tab:eq(' + index + ')', container).addClass('active');
+		};
+		$('.jstab_page', container).each(function(index) {
+			var title = $('.jstab_title', $(this));
+			var page = $(this);
+			title.hide();
+
+			// create tab for the page
+			var tab = $('<a class="jstab_tab">' + title.html() + '</a>');
+			tab.click(function() { switchToPage(index); });
+			tab_container.append(tab);
+
+			// hide the current tab
+			$(this).hide();
+		});
+
+		// prepend the tab container to the parent container
+		container.prepend(tab_container);
+
+		// show initial tab
+		switchToPage(0);
+	};
+
+	$('.jstab_container').each(function() {
+		init_tabarea($(this));
+	});
+}
